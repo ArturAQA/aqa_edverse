f120212520ef7a3807f12d69826b28c0bafc8693
ArturAQA <arturrmen@mail.ru>
2026-09-07 20:24:20 +0300

    выполнил дз: заполнил локаторы для главной страницы https://edversemovie.ru/

---
+++ locators/main_locators.py
@@ -0,0 +1,10 @@
+class MainLocators:
+    """Локаторы для главной страницы Cinescope"""
+    # Кнопки
+    AUTO_BUTTON = "[data-qa-id='login_page_button']"
+    SHOW_MORE_BUTTON = ("button", "Показать еще")
+    READ_MORE = "[data-qa-id='more_button']"
+    # Переход на страницу со всеми фильмами
+    ALL_MOVIES = ("link", "Все фильмы")
+    # Перход на главную страницу сайта
+    MAIN_PAGE = ("link", "Cinescope")
