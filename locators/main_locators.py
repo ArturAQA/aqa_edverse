a1d28cd77c1afa3cf56e73790ec8034efcec9054
ArturAQA <arturrmen@mail.ru>
2026-09-10 14:51:57 +0300

    fix: добавил локаторы для главной страницы по фидбеку ментора

--- locators/main_locators.py
+++ locators/main_locators.py
@@ -1,9 +1,17 @@
 class MainLocators:
     """Локаторы для главной страницы Cinescope"""
-    # Кнопки
+    # Кнопки общего интерфейса
     AUTO_BUTTON = "[data-qa-id='login_page_button']"
     SHOW_MORE_BUTTON = ("button", "Показать еще")
-    READ_MORE = "[data-qa-id='more_button']"
+    # Заголовок "Последние фильмы"
+    LAST_MOVIES = ("heading", "Последние фильмы")
+    # Карточка фильма
+    MOVIE_CARD = ".bg-card.text-card-foreground"
+    # Элементы внутри карточки фильма
+    MOVIE_TITLE = "heading"
+    MOVIE_POSTER = "img"
+    MOVIE_DESCRIPTION = "paragraph"
+    READ_MORE_BUTTON = "[data-qa-id='more_button']"
     # Переход на страницу со всеми фильмами
     ALL_MOVIES = ("link", "Все фильмы")
     # Перход на главную страницу сайта
