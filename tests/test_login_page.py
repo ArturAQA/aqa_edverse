b9ec825ecdfc2b300eb6473c612701d94d4fa6ca
ArturAQA <arturrmen@mail.ru>
2026-09-23 21:04:19 +0300

    feat: добавил параметризованный негативный тест логина

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,3 +1,4 @@
+import pytest
 import allure
 from playwright.sync_api import Page, expect
 from pages.login_page import LoginPage
@@ -23,3 +24,21 @@ def test_login_with_wrong_password(page: Page):
 
     with allure.step("Проверяем URL (остались на логине)"):
         expect(page).to_have_url("https://edversemovie.ru/login")
+        
+
+
+@pytest.mark.parametrize("email, password", [
+    pytest.param("artur_error_mail", "validPass123", id="почта_без_собачки"),
+    pytest.param("test@mail.ru", "1", id="пароль_из_одного_символа"),
+    pytest.param("", "", id="полностью_пустая_форма")
+])
+@allure.title("Негативный тест логина: {email}")
+@allure.feature("Авторизация")
+def test_login_negative_variants(page: Page, email, password):
+    login_page = LoginPage(page).open("https://edversemovie.ru/login")
+    
+    with allure.step("Вводим тестовые данные в форму"):
+        login_page.login(email, password)
+        
+    with allure.step("Проверяем, что сайт заблокировал вход и оставил нас на странице логина"):
+         expect(page).to_have_url(("https://edversemovie.ru/login"))   
